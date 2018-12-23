0e759ee7aeeb3ba9292be564df4f642955258b3a
Adrian Rocamora <adrianrocamorav@gmail.com>
2018-12-23 16:16:18 -0600

    Ginput example

--- py/matplotlib/ginput.py
+++ py/matplotlib/ginput.py
@@ -5,7 +5,11 @@ t = np.arange(10)
 plt.plot(t, np.sin(t))
 print("Please click")
 
-# Get two clicks from the user (will only render 1 mark)
-x = plt.ginput(2)
+# Get unlimited clicks
+x = plt.ginput(0)
 print("Clicked", x)
 plt.show()
+
+# middle mouse button stops input
+# left mouse button adds input
+# right mouse button pops previous input
