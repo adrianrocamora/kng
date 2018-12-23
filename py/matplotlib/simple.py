272890f74c213fb5d97970f313354f5372909f3b
Adrian Rocamora <adrianrocamorav@gmail.com>
2018-12-23 14:53:07 -0600

    Styling plotting tweaks with matplotlib and seaborn

--- py/matplotlib/simple.py
+++ py/matplotlib/simple.py
@@ -4,9 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set(style="darkgrid")
+# sns.set()
+sns.set(style='darkgrid')
 
-plt.xkcd()
+# Scale plot elements
+sns.set(context='notebook')    # notebook, paper, talk, poster
+# TODO: Fix margins in some plots? 
+
+# plt.xkcd()
 
 t = np.arange(0.0, 2.0, 0.01)
 s = np.sin(2*np.pi*t)
@@ -15,6 +20,6 @@ plt.plot(t, s)
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
-plt.grid(True)
+# plt.grid(True)
 plt.savefig("test.png")
 plt.show()
