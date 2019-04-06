3cc33dad12acfc0039e65e570cee5e86c38ca708
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-04-06 13:21:41 -0600

    P3 Working

--- gcj/2019/q/3.py
+++ gcj/2019/q/3.py
@@ -34,20 +34,20 @@ for i in range(1, t + 1):
     primes.insert(0, crymsg[0] // primes[0])
     primes.append(crymsg[l-1]//primes[l-1])
     primes.sort()
-    print(primes)
+    #print(primes)
     primes_set = set(primes)
-    print(primes_set)
-    print(len(primes_set))
+    #print(primes_set)
+    #print(len(primes_set))
     prim = list(primes_set)
-    print(prim)
-    print(len(prim))
+    #print(prim)
+    #print(len(prim))
 
     abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     prime_to_char = { prim[x] : abc[x] for x in range(26)}
     #char_to_prime = { abc[x] : primes[x] for x in range(26)}
 
-    print(prime_to_char)
+    #print(prime_to_char)
     #print(char_to_prime)
 
     def get_prime_pair(num, primes):
@@ -61,13 +61,35 @@ for i in range(1, t + 1):
     tuple_list = []
     for c in crymsg:
         tuple_list.append(get_prime_pair(c, prim))
-#    print(tuple_list)
+    #print(tuple_list)
+
+    def get_common_value_from_tuples(t1, t2):
+        if t1[0] == t2[0]:
+            return t1[0]
+        elif t1[0] == t2[1]:
+            return t1[0]
+        elif t1[1] == t2[0]:
+            return t1[1]
+        elif t1[1] == t2[1]:
+            return t1[1]
+        else:
+            pass
+
+    def get_different(t1, num):
+        if t1[0] != num:
+            return t1[0]
+        elif t1[1] != num:
+            return t1[1]
+        else:
+            pass
 
     prime_msg = []
-    for t in tuple_list:
-        prime_msg.append(t[0])
-    prime_msg.append(tuple_list[-1][1])
-    print(prime_msg)
+    for x in range(len(tuple_list)-1):
+        prime_msg.append(get_common_value_from_tuples(tuple_list[x], tuple_list[x+1]))
+    prime_msg.insert(0, get_different(tuple_list[0], prime_msg[0]))
+    prime_msg.append(get_different(tuple_list[-1], prime_msg[-1]))
+
+    #print(prime_msg)
 
     msg_li = []
     for p in prime_msg:
