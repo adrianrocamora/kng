67238e2e86330cb795c06b1d35190eda0bfc042a
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-05-07 14:12:51 -0500

    Web scraping container data

---
+++ py/scraping/simple.py
@@ -0,0 +1,15 @@
+import bs4  # Good for traversing html text 
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38'
+#print(my_url)
+uClient = uReq(my_url)
+page_html = uClient.read()
+#print(page_html)
+uClient.close()
+
+page_soup = soup(page_html, "html.parser")
+containers = page_soup.findAll("div", {"class": "item-info"})
+print(len(containers))
+print(containers[0])
