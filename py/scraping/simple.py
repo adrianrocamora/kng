1ba94f62b42b45366b693f19899fc3c8b62bbf8d
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-05-07 14:22:42 -0500

    Iterate over all containers

--- py/scraping/simple.py
+++ py/scraping/simple.py
@@ -10,9 +10,15 @@ page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
-containers = page_soup.findAll("div", {"class": "item-info"})
-print(len(containers))
-print(containers[0].div.a.img["title"])
-print(containers[0].findAll("a", {"class":"item-title"})[0].text)
-print(containers[0].findAll("li", {"class":"price-ship"})[0].text.strip())
 
+containers = page_soup.findAll("div", {"class": "item-info"})
+
+for container in containers:
+	brand = container.div.a.img["title"]
+	product_name = container.findAll("a", {"class":"item-title"})[0].text
+	shipping = container.findAll("li", {"class":"price-ship"})[0].text.strip()
+
+	print(brand)
+	print(product_name)
+	print(shipping)
+	print('')
