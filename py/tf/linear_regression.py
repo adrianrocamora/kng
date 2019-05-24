f56d9b2b36c5724cde30e56207785ccd2496f628
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-05-23 22:23:13 -0500

    Fixed python format

--- py/tf/linear_regression.py
+++ py/tf/linear_regression.py
@@ -9,10 +9,11 @@ training_epochs = 1000
 display_step = 50
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+train_X = numpy.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59,
+                         2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+train_Y = numpy.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596,
+                         2.53, 1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94,
+                         1.3])
 n_samples = train_X.shape[0]
 
 # tf Graph Input
@@ -44,17 +45,18 @@ with tf.Session() as sess:
         for (x, y) in zip(train_X, train_Y):
             sess.run(optimizer, feed_dict={X: x, Y: y})
 
-        #Display logs per epoch step
+        # Display logs per epoch step
         if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-                "W=", sess.run(W), "b=", sess.run(b)
+            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),
+                  "W=", sess.run(W), "b=", sess.run(b))
 
-    print "Optimization Finished!"
+    print("Optimization Finished!")
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print "Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n'
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=",
+          sess.run(b), '\n')
 
-    #Graphic display
+    # Graphic display
     plt.plot(train_X, train_Y, 'ro', label='Original data')
     plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
     plt.legend()
