4e232273c17acead7b66215ef339c512f81d4456
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-06-01 20:08:00 -0500

    Adjusting image panel margins

--- py/pillow/combine.py
+++ py/pillow/combine.py
@@ -1,10 +1,12 @@
 # Create a new image from two images
-
 import os
 import random
 
+panel_w = 700
+panel_h = 1000
+
 from PIL import Image, ImageDraw
-img_wp = Image.new('RGB', (1400, 1000), color='black')
+img_wp = Image.new('RGB', (panel_w*2, panel_h), color='black')
 
 path = '/home/rocamora/wallpapers/x'
 
@@ -19,9 +21,22 @@ random_choices = random.sample(files, k=2)
 print(random_choices)
 
 img_left = Image.open(random_choices[0])
-img_left.thumbnail((700, 1000))
+img_left.thumbnail((panel_w, panel_h))
 img_right = Image.open(random_choices[1])
-img_right.thumbnail((700, 1000))
+img_right.thumbnail((panel_w, panel_h))
+
+img_left_x0 = 0 + (panel_w - img_left.width)//2
+img_left_y0 = 0 + (panel_h - img_left.height)//2
+
+img_right_y0 = 0 + (panel_h - img_right.height)//2
+img_right_x0 = panel_w + (panel_w - img_right.width)//2
+
+img_wp.paste(img_left, (img_left_x0, img_left_y0))
+img_wp.paste(img_right, (img_right_x0, img_right_y0))
+
+img_wp.save('/home/rocamora/wallpapers/wp.png')
+
+
 
 # #Iterate through a 4 by 4 grid with 100 spacing, to place my image
 # for i in range(0,1400,700):
@@ -30,9 +45,3 @@ img_right.thumbnail((700, 1000))
         # #img_wp = Image.eval(im,lambda x: x+(i+j)/30)
         # #paste the image at location i,j:
         # img_wp.paste(im, (i,j))
-
-print(img_left.width)
-img_wp.paste(img_left, (0,0))
-img_wp.paste(img_right, (700,0))
-
-img_wp.save('/home/rocamora/wallpapers/wp.png')
