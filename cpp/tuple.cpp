f1ee3d310ea3c4c4808584956908595f188febbb
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-10 09:33:51 -0600

    code styling

--- cpp/tuple.cpp
+++ cpp/tuple.cpp
@@ -9,6 +9,7 @@ int main() {
 	cout << get<0>(tup1) << endl;
 	cout << get<1>(tup1) << endl;
 	cout << get<2>(tup1) << endl;
+
 	tuple<char, int, string> tup2 = make_tuple('B', 456, "qwre");
 	cout << tuple_size<decltype(tup2)>::value << endl;
 	cout << get<0>(tup2) << endl;
