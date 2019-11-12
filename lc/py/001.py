414d8bc35245f529d536a992b1f65a2af325f6c2
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-12 09:08:14 -0600

    Leetcode 001 problem

---
+++ lc/py/001.py
@@ -0,0 +1,9 @@
+t1a = [2, 7, 11, 15]
+t1t = 9
+
+# find the indices of the numbers that add up to the target
+
+arr = t1a
+tar = t1t
+for idx in range(0, len(arr)):
+  print(str(idx) + " " + str(arr[idx]))
