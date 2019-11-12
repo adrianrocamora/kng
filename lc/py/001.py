2b09cf82f35b030f1a85e0146073ad48f2c6d42e
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-12 09:45:07 -0600

    Leetcode 001 wip

--- lc/py/001.py
+++ lc/py/001.py
@@ -1,3 +1,6 @@
+def sort(arr):
+    return arr
+
 t1a = [2, 7, 11, 15]
 t1t = 9
 
@@ -6,4 +9,14 @@ t1t = 9
 arr = t1a
 tar = t1t
 for idx in range(0, len(arr)):
-  print(str(idx) + " " + str(arr[idx]))
+    print(str(idx) + " " + str(arr[idx]))
+
+print("O(n**2) - sort and search")
+
+arr = sort(arr)
+for i in range(0, len(arr)):
+    for j in range(0, len(arr)):
+        if i != j:
+            print(str(i) +", " + str(j))
+
+
