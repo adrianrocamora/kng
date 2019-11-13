f92d644e7b7c240f544ec91680109445432c197f
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-12 22:08:57 -0600

    LC001 basic solution

--- lc/py/001.py
+++ lc/py/001.py
@@ -6,17 +6,18 @@ t1t = 9
 
 # find the indices of the numbers that add up to the target
 
-arr = t1a
-tar = t1t
-for idx in range(0, len(arr)):
-    print(str(idx) + " " + str(arr[idx]))
-
 print("O(n**2) - sort and search")
 
-arr = sort(arr)
-for i in range(0, len(arr)):
-    for j in range(0, len(arr)):
-        if i != j:
-            print(str(i) +", " + str(j))
+def solution(arr, tar):
+    arr = sort(arr)
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if i < j:
+                if arr[i] + arr[j] == tar:
+                    return [i,j]
+
+print("Result is:")
+idxs = solution(t1a, t1t)
+print(idxs)
 
 
