a8c8c3d27bf9d073c8cf1da0d74928d539f6f815
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-13 15:47:54 -0600

    LC001 solution

--- lc/py/001.py
+++ lc/py/001.py
@@ -2,13 +2,13 @@ def sort(arr):
     return arr
 
 t1a = [2, 7, 11, 15]
-t1t = 9
+t1t = 22
 
 # find the indices of the numbers that add up to the target
 
 print("O(n**2) - sort and search")
 
-def solution(arr, tar):
+def solution_slow(arr, tar):
     arr = sort(arr)
     for i in range(0, len(arr)):
         for j in range(0, len(arr)):
@@ -17,7 +17,24 @@ def solution(arr, tar):
                     return [i,j]
 
 print("Result is:")
-idxs = solution(t1a, t1t)
+idxs = solution_slow(t1a, t1t)
 print(idxs)
 
 
+print("")
+print("O(n*log(n)) - hashmap of addendum to idx")
+
+def solution(arr, tar):
+    #cdi = { tar - i : [
+    di = dict()
+    for idx in range(0, len(arr)):
+        di[tar - arr[idx]] = idx
+    for idx in range(0, len(arr)):
+        if arr[idx] in di:
+            idx2 = di[arr[idx]]
+            return [idx, idx2] if idx < idx2 else [idx2, idx]
+    return None
+
+print("Result is:")
+idxs = solution(t1a, t1t)
+print(idxs)
