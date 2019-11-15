7564a0458c2b8d64c3dd9476463d1cae8bcd9a2d
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-15 08:51:40 -0600

    Code reorg

--- lc/py/001.py
+++ lc/py/001.py
@@ -4,12 +4,15 @@ def sort(arr):
 t1a = [2, 7, 11, 15]
 t1t = 22
 
+t1a = [3, 2, 4]
+t1t = 6
+
 # find the indices of the numbers that add up to the target
 
 print("O(n**2) - sort and search")
 
 def solution_slow(arr, tar):
-    arr = sort(arr)
+    arr.sort()
     for i in range(0, len(arr)):
         for j in range(0, len(arr)):
             if i < j:
