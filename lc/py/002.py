312cf7685fdfc1eb9800b42752817b854594bff6
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-11-18 15:46:52 -0600

    Updates

---
+++ lc/py/002.py
@@ -0,0 +1,14 @@
+inp = [[2, 4, 3] , [5, 6, 4]]
+out = [7, 0, 8]
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+  def __init__(self, val, nxt):
+    self.val = val
+    self.nxt = nxt
+
+
+
