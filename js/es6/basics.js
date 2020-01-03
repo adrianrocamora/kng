95e241741b185b858199d3560b9ecf777ba71f1a
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-01-02 20:10:17 -0600

    Basic ES6 syntax

---
+++ js/es6/basics.js
@@ -0,0 +1,14 @@
+let age = 24;
+const name = 'Mai';
+
+for (let i = 0; i < 3; i++) {
+	console.log('Running from the for loop');
+}
+
+const addNums = (a, b) => {
+	return a + b;
+}
+
+console.log(addNums(5, 10));
+
+age < 30 ? console.log("Younger than 30") : console.log('Older than 30');
