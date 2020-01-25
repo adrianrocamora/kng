3910c9f0aa8ba91531ac693339b26ae5c9c3f51a
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-01-25 14:48:33 -0600

    Reverse integer

--- lc/py/007.py
+++ lc/py/007.py
@@ -1,10 +1,24 @@
+# 007 Reverse Integer
+
+# Reverse the digits of an integer
+
+# Repeatedly multiply the previous result by 10 and add the last digit
+# Time - O(n) where n is the number of digits
+# Space - O(n) same number of digits in output as input
+
 class Solution(object):
+  def reverse_simple(self, x):
+    xs = str(x)
+    xs = xs[::-1]
+    x = int(xs)
+    return x
+    
   def reverse(self, x):
     negative = x < 0
     x = abs(x)
     reversed = 0
 
-    while x !=0:
+    while x != 0:
       reversed = reversed * 10 + x % 10
       x //= 10
     
@@ -12,7 +26,6 @@ class Solution(object):
       return 0
     return reversed if not negative else -reversed
 
-sol = Solution()
 x = 1234
 print(x)
-print(sol.reverse(x))
+print(Solution().reverse(x))
