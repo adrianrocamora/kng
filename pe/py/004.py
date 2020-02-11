ae49e08cc683e358a53cc54b3c10bd7777b05331
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-02-10 23:40:18 -0600

    Faster solution

--- pe/py/004.py
+++ pe/py/004.py
@@ -21,34 +21,41 @@ def largest_palindromic_num_O_n_2(n):
   return max_prod
 
 def largest_palindromic_num(n):
+
+  # P = m n
+  # P = 100,001 a + 10,010 b + 1100 c
+  # P = 11(9091 a + 910 b + 100 c
+  # So either a or b have a factor of 11
+
+  # We can iterate over less numbers now
+  # Previously (999 - 99) ** 2 == 810,000
+  # Now 10*10*10 = 1000
+  # 
+
   def is_palindrome(num):
     num_str = str(num)
     return num_str == num_str[::-1]
 
-  start = 10 ** (n - 1)
-  finish = 10 ** n - 1 
+  # We iterate over all possible palindromes
+  # and we check if they are a product of 3 digit
+  # numbers one of which is a multiple of 11
 
   max_prod = 0
-  for p1 in range(star, (start + finish + 1)//2):
-    max_prod_local = 0
-    lo = p1
-    hi = finish
-    mid = (lo + hi) // 2
-
-    while lo < hi:
-      curr_prod = p1 * 
-      if p1 * mid > max_prot
-
-    for p2 in range(start, finish + 1):
-      prod = p1 * p2
-      if is_palindrome(prod):
-        if prod > max_prod:
-          max_prod = prod
-
+  for a in range(0, 10):
+    for b in range(0, 10):
+      for c in range(0, 10):
+        candidate = 100001*a + 10010*b + 1100*c
+        for m in range(110, 990):
+          if candidate % m == 0 and \
+              100 < candidate // m < 999 and \
+              candidate > max_prod:
+            max_prod = candidate
   return max_prod
   
-print(largest_palindromic_num(2))
-print()
+#print(largest_palindromic_num(2))
+#print()
+#print(largest_palindromic_num_O_n_2(3))
+
 print(largest_palindromic_num(3))
-print()
+#print()
 #print(largest_palindromic_num(4))
