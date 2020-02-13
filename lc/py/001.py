89883e3af76f5741df002c4bc415b1ba67fbd82c
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-02-13 11:48:48 -0600

    Updates

--- lc/py/001.py
+++ lc/py/001.py
@@ -1,5 +1,4 @@
 # 001 - Two Sum
-
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution
 
@@ -8,44 +7,42 @@
 # Time - O(n)
 # Space - O(n) for the dictionary
 
-class Solution(object):
-  def two_sum_best(self, nums, target):
-    num_to_idx = {}
+def two_sum_best(nums, target):
+  num_to_idx = {}
 
-    for i, num in enumerate(nums):
-      if target - num in num_to_idx:
-        res = [i, num_to_idx[target - num]]
-        res.sort()
-        return res
-      
-      num_to_idx[num] = i
+  for i, num in enumerate(nums):
+    if target - num in num_to_idx:
+      res = [i, num_to_idx[target - num]]
+      res.sort()
+      return res
+    
+    num_to_idx[num] = i
 
-    return []
+  return []
 
-  def two_sum(self, nums, target):
-    num_to_idx = {}
+def two_sum(nums, target):
+  num_to_idx = {}
 
-    for i, num in enumerate(nums):
-      num_to_idx[num] = i
+  for i, num in enumerate(nums):
+    num_to_idx[num] = i
 
-    for i, num in enumerate(nums):
-      if target - num in num_to_idx:
-        res = [i, num_to_idx[target - num]]
-        res.sort()
-        return res
-    return []
+  for i, num in enumerate(nums):
+    if target - num in num_to_idx:
+      res = [i, num_to_idx[target - num]]
+      res.sort()
+      return res
+  return []
 
 
-sol = Solution()
 nums = [6, 9, 2, 3]
 target = 11 
 print(nums)
 print(target)
-print(sol.two_sum(nums, target))
+print(two_sum(nums, target))
 print()
 
 nums = [2, 3, 6, 9]
 target = 8
 print(nums)
 print(target)
-print(sol.two_sum(nums, target))
+print(two_sum(nums, target))
